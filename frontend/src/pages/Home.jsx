3e3db25ba1acf50b873409b18424d7fc472abd59
Aditya Singh Mandloi <108330859+adityasinghmandloi@users.noreply.github.com>
2024-09-28 00:26:40 +0530

    third commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
-
-
+import BestSeller from "../components/BestSeller";
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
 const Home = () => {
   return (
     <div>
-      
+      <LatestCollection></LatestCollection>
+      <BestSeller></BestSeller>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
