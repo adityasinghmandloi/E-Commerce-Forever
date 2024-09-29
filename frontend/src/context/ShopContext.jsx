275aece423a996840afa0fff32375e65dabf442c
Aditya Singh Mandloi <108330859+adityasinghmandloi@users.noreply.github.com>
2024-09-29 12:17:35 +0530

    commit

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
@@ -6,6 +6,9 @@ export const ShopContext = createContext();
 const ShopContextProvider = ({ children }) => {
   const currency = "$";
   const deliveryFee = 10; // changed to camelCase for consistency
+
+  const [search, setSearch] = useState(" ");
+  const [showSearch, setshowSearch] = useState(true);
 
   const value = {
     products,
